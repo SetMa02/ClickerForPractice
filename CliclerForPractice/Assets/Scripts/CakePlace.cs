a0188a8934e7e9de985c18c555dca7e7d6ead25e
Максим Майнагашев <maxuser99@mail.ru>
2021-11-14 23:42:21 +0700

    Scene Setted
    Добавлена базовая сцена с тортом

---
+++ CliclerForPractice/Assets/Scripts/CakePlace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakePlace : MonoBehaviour
+{
+    [SerializeField] private GameObject _cakePrefab;
+
+    private void Start()
+    {
+        SetCake(_cakePrefab);
+    }
+
+    public void SetCake(GameObject cake)
+    {
+        _cakePrefab = Instantiate(cake, transform);
+    }
+
+    public void RemoveCake(GameObject cake)
+    {
+        Destroy(cake);
+    }
+}
